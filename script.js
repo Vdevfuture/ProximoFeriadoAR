65599764158f58419ff0a91a6591a3d9569bc4ff
Yorman Villasmil <98121835+Vdevfuture@users.noreply.github.com>
2023-11-06 17:02:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-const password = "ucasal123";
+import { obtenerJson, calcularProximoFeriado } from "https://cdn.esjs.dev/KYDwDg9gTgLghlABAMwK4DsDGBLC7GZwA2mqRCAClBCNgLYQBiwU2cAJhABTItucBnAJSIA3gChEBPAJwI4AC2hxEAXgJRgCRM0wK4XIZOnpZ8gI8QAgphipia+UqhwAdBABGMYOhZXLhsaYMnLsbDZ2DuqKyu5ePiwAImyBUsGmcnTAAhH2RI4xLnHevlAAstmGiADUiACMxpow0OjavKwcEAKuyNjo7FzGUjx8nSKqAHxDUogjHZyuWQJqqupLuQ4AZJsoowthKhOIBxtEIgA+59NS7fwQi9mIR+u2ecZGAL5AA";
 
-const loginButton = document.getElementById("loginButton");
-const passwordInput = document.getElementById("password");
-const feriadoInfo = document.getElementById("feriadoInfo");
-const motivo = document.getElementById("motivo");
-const fecha = document.getElementById("fecha");
+const password = "ucasal123"; // password
 
-loginButton.addEventListener("click", () => {
-  const enteredPassword = passwordInput.value;
-  if (enteredPassword === password) {
-    feriadoInfo.style.display = "block";
-    passwordInput.value = "";
-    mostrarProximoFeriado();
-  } else {
-    alert("Contraseña incorrecta");
+const obtenerFeriados = async (year) => {
+  const url = `https://nolaborables.com.ar/api/v2/feriados/${year}`;
+  try {
+    const feriados = await obtenerJson(url);
+    return feriados;
+  } catch (error) {
+    throw error;
   }
-});
+};
 
-function mostrarProximoFeriado() {
-  fetch("https://nolaborables.com.ar/api/v2/feriados/2023")
-    .then((response) => response.json())
-    .then((data) => {
-      const proximoFeriado = data[0];
-      motivo.textContent = `Motivo: ${proximoFeriado.motivo}`;
-      fecha.textContent = `Fecha: ${proximoFeriado.fecha}`;
-    })
-    .catch((error) => {
-      console.error(error);
+const mostrarProximoFeriado = async () => {
+  try {
+    const year = 2023; // Change the year as needed
+    const feriados = await obtenerFeriados(year);
+
+    // Request password
+    const passwordInput = document.getElementById("password");
+    const loginButton = document.getElementById("loginButton");
+
+    loginButton.addEventListener("click", () => {
+      const enteredPassword = passwordInput.value.trim();
+      if (enteredPassword === password) {
+        const proximoFeriado = calcularProximoFeriado(feriados);
+
+        // Show the next holiday
+        document.getElementById("motivo").textContent = `Holiday: ${proximoFeriado.motivo}`;
+        document.getElementById("fecha").textContent = `Date: ${proximoFeriado.dia}/${proximoFeriado.mes}`;
+        document.getElementById("feriadoInfo").style.display = "block";
+
+        // Hide the login form
+        document.getElementById("loginForm").style.display = "none";
+      } else {
+        alert("Incorrect password. You cannot view the information.");
+      }
     });
-}
+  } catch (error) {
+    console.error("Error fetching the next holiday:", error);
+  }
+};
+
+// Call the function to show the next holiday
+mostrarProximoFeriado();
